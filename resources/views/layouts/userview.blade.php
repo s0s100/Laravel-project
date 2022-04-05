6bc20f21812e5f338e30adb8e6359493eb9eae79
s0s100 <s0s100@mail.ru>
2022-04-05 16:32:09 +0100

    Added posts and comments

--- resources/views/layouts/userview.blade.php
+++ resources/views/layouts/userview.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <title> @yield('username')s page </title>
     <meta charset="utf-8">
@@ -8,7 +9,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
-    <link rel='stylesheet' href="{{URL::asset('css/mycss.css')}}">
+    <link rel='stylesheet' href="{{ URL::asset('css/mycss.css') }}">
 </head>
 
 <body>
@@ -17,21 +18,52 @@
             <div class="column">
                 <div class="row-sm-4">
                     <h1>@yield('username')</h1>
-                    <img src="{{asset('images/test_user_image.jpg')}}" class="img-circle" alt = "User pic">
+                    <img src="{{ asset('images/test_user_image.jpg') }}" class="img-circle" alt="User pic">
                 </div>
                 <div class="row-sm-4" style="background-color:lavender;">
-                    <h1> User information: </h1 >
+                    <h1> User information: </h1>
                     <div>
                         @yield('content')
                     </div>
                     <a> Followers: @yield('followers') </a>
-                    <a> Following: @yield('following') </a> 
+                    <a> Following: @yield('following') </a>
+                </div>
+                <div class="row-sm-4">
+                    <h2> Comments: </h2>
+                    @foreach ($user->comments as $comment)
+                        <div class="row-sm-2 comment">
+                            <b> Created by: {{ $comment->user->name }} </b>
+                            <br>
+                            <b> Under the post of: {{$comment->post->user->name}} </b>
+                            <br>
+                            <small> {{ $comment->text }} </small>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
         <div class="col-sm-8" colo>
-            <h1> Posts </h1>
+            <div class="column">
+                <h1> Posts </h1>
+                @foreach ($user->posts as $post)
+                    <div class="row-sm-4">
+                        <b> Created by: {{ $post->user->name }} </b>
+                        <br>
+                        <a> {{ $post->text }} </a>
+                        <br>
+                        <i> Comments: </i>
+                        @foreach ($post->comments as $comment)
+                            <div class="row-sm-2 comment">
+                                <b> Created by: {{ $comment->user->name }} </b>
+                                <br>
+                                <small> {{ $comment->text }} </small>
+                            </div>
+                        @endforeach
+                    </div>
+                @endforeach
+            </div>
         </div>
     </div>
 </body>
+
 </html>
